91d1ac04aa72f4a7c04a94fd59c41b8ea259e13d
Alexsander Akers <a2@pnd.mn>
2014-05-21 00:28:52 +0100

    Update comments, error handling

--- app.rb
+++ app.rb
@@ -37,12 +37,14 @@ def consumer
   end
 end
 
+# Format the card's balance
 def balance(card)
   balance = card['balance']
   currency = card['balanceCurrencyCode']
   Monetize.parse(balance, currency).format
 end
 
+# Convert a Starbucks "date" into a UNIX timestamp.
 def date_to_i(date)
   matches = /\/Date\((\w*)([+-]\d{4})\)\//.match(date)
   if match = matches[1]
@@ -53,24 +55,37 @@ def date_to_i(date)
 end
 
 def pebble_barcode(card, access_token)
-  # Get the barcode.
-  url = "https://api.starbucks.com/barcode/v1/mobile/payment/starbuckscard/#{card}?engine=onbarcode&height=85&width=200"
+  # Get the barcode
+  url = "https://api.starbucks.com/barcode/v1/mobile/payment/starbuckscard/" + \
+    "#{card}?engine=onbarcode&height=85&width=200"
   data = access_token.get(url)
 
   ds = ChunkyPNG::Datastream.from_blob(data.body)
   image = ChunkyPNG::Image.from_datastream(ds)
+
+  # Trim the whitespace
   image.trim!
+
+  # Resize
   image.resample_nearest_neighbor!(142, 8)
+
+  # Convert to .pbi format
   image.to_pbi
 end
 
 def me_data(access_token)
-  # Get the profile data.
+  # Get the profile data
   url = 'https://api.starbucks.com/starbucksprofile/v1/users/me'
-  data = access_token.get(url, { 'Accept' => 'application/json' })
-  json = JSON.parse(data.body)
+  response = access_token.get(url, { 'Accept' => 'application/json' })
 
-  # Get the interesting card data.
+  # Try to parse the result JSON
+  begin
+    json = JSON.parse(response.body)
+  rescue JSON::ParserError => e
+    halt response.code
+  end
+
+  # Get the interesting card data
   cards = json['starbucksCards'].map do |card|
     {
       balance: balance(card),
@@ -85,18 +100,25 @@ def me_data(access_token)
 end
 
 def rewards_data(access_token)
-  # Get the rewards data.
+  # Get the rewards data
   url = 'https://api.starbucks.com/starbucksprofile/v1/users/me/rewards'
-  data = access_token.get(url, { 'Accept' => 'application/json' })
-  json = JSON.parse(data.body)
+  response = access_token.get(url, { 'Accept' => 'application/json' })
+
+  # Try to parse the result JSON
+  begin
+    json = JSON.parse(response.body)
+  rescue JSON::ParserError => e
+    halt response.code
+  end
 
   threshold = json['starsThresholdForFreeDrink'].to_i
   stars_left = json['starsNeededForNextFreeDrink'].to_i
 
-  # Get the number of stars until the threshold.
+  # Get the number of stars until the threshold
   stars = threshold - stars_left
   stars = 0 if stars < 0
 
+  # Form the result data
   {
     updated_at: date_to_i(json['dateRetrieved']),
     stars: stars,
@@ -109,83 +131,90 @@ get '/' do
 end
 
 get '/login' do
-  # Get a request token from Starbucks.
+  # Get a request token from Starbucks
   callback = "#{base_url}/callback"
   request_token = consumer.get_request_token(oauth_callback: callback)
 
-  # Store both the token and secret in the session.
+  # Store both the token and secret in the session
   session[:request_token] = request_token.token
   session[:request_secret] = request_token.secret
 
-  # Redirect to Starbucks for authorization.
+  # Redirect to Starbucks for authorization
   redirect request_token.authorize_url(oauth_callback: callback)
 end
 
 get '/callback' do
-  # Get the token and secret we stored in the session.
+  # Get the token and secret we stored in the session
   token = session[:request_token]
   secret = session[:request_secret]
 
-  # If we don't have both, someone done goofed.
+  # If we don't have both, someone done goofed
   halt 400 unless token and secret
   session[:request_token] = session[:request_secret] = nil
 
-  # Reconstruct the request token.
+  # Reconstruct the request token
   request_token = OAuth::RequestToken.from_hash(
     consumer,
     oauth_token: token,
     oauth_token_secret: secret
   )
 
-  # Get the access token from the request token and the query params.
+  # Get the access token from the request token and the query params
   verifier = params[:oauth_verifier]
   access_token = request_token.get_access_token(oauth_verifier: verifier)
 
-  # Construct the response data, and go.
+  # Construct the response data, and go
   data = {
     access_token: access_token.token,
     access_token_secret: access_token.secret,
   }
   fragment = URI.encode_www_form(data)
-  url = URI::Generic.build(scheme: 'pebblejs', host: 'close', fragment: fragment)
+
+  # Form the URL
+  url = URI::Generic.build(
+    scheme: 'pebblejs',
+    host: 'close',
+    fragment: fragment
+  )
+
+  # Redirect to "pebblejs://close#..."
   redirect url.to_s
 end
 
 post '/data' do
-  # Reconstruct the access token from the query params.
+  # Reconstruct the access token from the query params
   access_token = OAuth::AccessToken.from_hash(
     consumer,
     oauth_token: params[:access_token],
     oauth_token_secret: params[:access_token_secret]
   )
 
-  begin
-    response_data = me_data(access_token)
-    response_data['rewards'] = rewards_data(access_token)
+  response_data = me_data(access_token)
+  response_data['rewards'] = rewards_data(access_token)
 
-    content_type :json
-    JSON.generate(response_data)
-  rescue
-    halt 400
-  end
+  content_type :json
+  JSON.generate(response_data)
 end
 
 post '/raw' do
-  # Reconstruct the access token from the query params.
+  # If we don't have both, someone done goofed
+  halt 401 unless params[:access_token] && params[:access_token_secret]
+
+  # Reconstruct the access token from the query params
   access_token = OAuth::AccessToken.from_hash(
     consumer,
     oauth_token: params[:access_token],
     oauth_token_secret: params[:access_token_secret]
   )
 
+  # Get the request URL
   url = params[:url]
   halt 400 unless url
 
-  request_headers = {}
-  request_headers['Accept'] = 'application/json' if params[:json]
+  # Make the request
+  response = access_token.get(url, { 'Accept' => 'application/json' })
 
-  response = access_token.get(url, request_headers)
-
+  # Return the results
   status response.code
   headers response.header.to_hash
   body response.body
