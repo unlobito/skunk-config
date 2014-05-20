890effa7533435f24155a54f91d9093f76f71443
Alexsander Akers <a2@pnd.mn>
2014-05-20 21:34:15 +0100

    Add 'env.sample.rb' file

---
+++ env.sample.rb
@@ -0,0 +1,10 @@
+# Copy this file to 'env.rb'
+
+# This file contains the ENV vars necessary to run the app locally.
+# Some of these values are sensitive, and some are developer specific.
+#
+# DO NOT CHECK THIS FILE INTO VERSION CONTROL
+
+ENV['SBUX_CONSUMER_KEY'] = ''
+ENV['SBUX_CONSUMER_SECRET'] = ''
+ENV['COOKIE_SECRET'] = ''
